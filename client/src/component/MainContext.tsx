bd2c0607387e7784b6a171862b440cf77a402e53
Nirav Dongre <nsd3284@gmail.com>
2026-05-11 22:29:43 +0530

    fixed Some bug

--- client/src/component/MainContext.tsx
+++ client/src/component/MainContext.tsx
@@ -1,27 +1,33 @@
- 
+import axios from "axios" 
+import { useState } from "react"
 
+function Input({onChange}){
 
-function Input(){
-    return <input onChange={(e) => {
-        const url = e.target.value
-        console.log(url)
-    }} className="bg-green-500  border-[3px] border-white w-full pl-5 p-4 text-xl rounded-none font-mono tracking-wider shadow-[4px_4px_0px_white]" type="text" placeholder="Youtube URL"/>
+    return <input onChange={onChange} className="bg-green-500  border-[3px] border-white w-full pl-5 p-4 text-xl rounded-none font-mono tracking-wider shadow-[4px_4px_0px_white]" type="text" placeholder="Youtube URL"/>
 }
 
 export default function MainContext(){
 
+    const [ Url, setUrl ] = useState()
 
+    const handler = async () => {
+        const response = await axios.post("http://localhost:3000/transcript",{
+            Input: Url
+        })
 
+        console.log(response.data)
+
+    }
     return (
         <div className="flex justify-center mt-16">
             <div>
              <h1 className="text-6xl font-bold p-7 text-red-900">Youtube to Transcript</h1>
              <p className="text-2xl p-5 text-red-500">Generate your Transcript for free using Youtube link.</p>
                <div className="p2 flex justify-between">
-                <Input></Input>
-                <button onClick={() => {
-                    axios
-                }} type="submit" className="p-5 ml-10 text-xl border  transition delay-50 duration-300 hover:bg-blue-600 rounded-xl bg-transparent">Get it...{">-<"}</button>
+                <Input onChange={(e: any) => {
+                     setUrl(e.target.value)
+                }}></Input>
+                <button onClick={handler} type="submit" className="p-5 ml-10 text-xl border  transition delay-50 duration-300 hover:bg-blue-600 rounded-xl bg-transparent">Get it...{">-<"}</button>
                </div>
             </div>
         </div>
