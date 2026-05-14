b1eeaaab4bffab2f40a9481a2b37cbf05d331699
Nirav Dongre <nsd3284@gmail.com>
2026-05-14 16:11:21 +0530

    Added the Sign-up page working for me now

--- client/src/component/Topbar.tsx
+++ client/src/component/Topbar.tsx
@@ -1,24 +1,29 @@
+import { useNavigate } from "react-router-dom"
 
-
-function TopBarButton({Bname}){
+export function TopBarButton({Bname, className}){
     return (
-        <div className="p-4">
+        <div className={className}>
             <button className="text-1xl hover:border border-white rounded p-2">{Bname}</button>
         </div>
     )
 }
 
 export default function TopBar(){
+
+    const navigate = useNavigate();
+
     return (
         <div className="flex justify-between">
             <div className="p-5 text-5xl font-bold">Bit</div>
 
             <div className="flex">
-                <a href="https://github.com/NiravDongre/Bit" ><TopBarButton Bname={"Github"}></TopBarButton></a>
-                <TopBarButton Bname={"Features"}></TopBarButton>
-                <TopBarButton Bname={"About"}></TopBarButton>
+                <a href="https://github.com/NiravDongre/Bit" ><TopBarButton className={"p-4"} Bname={"Github"}></TopBarButton></a>
+                <TopBarButton className={"p-4"} Bname={"Features"}></TopBarButton>
+                <TopBarButton className={"p-4"} Bname={"About"}></TopBarButton>
                 <div className="bg-pink-600 transition delay-150 hover:bg-black  flex ">
-                <button className="text-xl p-2">LogIn</button>
+                <button onClick={() => {
+                    navigate("/auth/sign-up")
+                }} className="text-xl p-2">LogIn</button>
                 </div>
             </div>
         </div>
--- client/src/pages/SignUpPage.tsx
+++ client/src/pages/SignUpPage.tsx
@@ -1,9 +1,55 @@
-
+import { useState } from "react"
+import axios from "axios"
 
 
 
 export function SingUpPage() {
+
+    const [ Username, setUsername ] = useState("")
+    const [ Email, setEmail ] = useState("")
+    const [ Password, setPassword ] = useState("")
+    
+     
+    console.log(Username)
     return (
-        <div>Hi there</div>
+        <div className="flex justify-center items-center h-screen p-16">
+
+            <div className="">
+
+            <h1 className="text-center font-mono text-3xl mb-5">Sign up</h1>
+
+
+            <div className="grid rounded-xl bg-teal-50 p-10">
+                <div>
+                <p className="text-xl p-2 text-left">username :</p>
+                <input onChange={(e) => { setUsername(e.target.value) }} className="p-2 bg-transparent text-xl rounded-xl border border-yellow" type="text" placeholder="username" />
+                </div>
+
+                <div className="mt-4 mb-4">
+                    <p className="text-xl p-2 text-left">email :</p>
+                    <input onChange={(e) => { setEmail(e.target.value) }} className="p-2 bg-transparent text-xl rounded-xl border border-yellow" type="text" placeholder="email" />
+                </div>
+
+                <div>
+                <p className="text-xl p-2 text-left">password :</p>
+                <input onChange={(e) => { setPassword(e.target.value) }} className="p-2 bg-transparent text-xl rounded-xl border border-yellow" type="text" placeholder="password" />
+                </div>
+
+             <button type="submit" onClick={async() => {
+                const response = await axios.post("http://localhost:3000/api/v2/auth/sign-up", {
+                    username: Username,
+                    email: Email,
+                    password: Password
+                })
+
+                console.log(response)
+             }} className="bg-black p-4 mt-4 rounded-xl text-xl">Sumbit</button>
+
+            </div>
+
+
+            </div>
+
+        </div>
     )
 }
