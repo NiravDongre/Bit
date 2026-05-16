9faad019af090a4625f56b0c2b057420e6929948
Nirav Dongre <nsd3284@gmail.com>
2026-05-16 08:59:58 +0530

    Adding Protected Routes

--- client/src/component/ProtectedRoute.tsx
+++ client/src/component/ProtectedRoute.tsx
@@ -1,13 +1,62 @@
+// src/components/ProtectedRoute.tsx
+
+import {  useEffect, useState, type ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
+type Status =
+  | "loading"
+  | "authenticated"
+  | "unauthenticated"
+  | "error";
 
+type ProtectedRouteProps = {
+  children: ReactNode;
+};
 
+export default function ProtectedRoute({
+  children,
+}: ProtectedRouteProps) {
+  const [status, setStatus] = useState<Status>("loading");
 
-export function ProtectedRoute({user}, {children}){
-    if(!user){
-        return <Navigate to="/auth/sign-in"/>
-    }
-    else{
-        return {children}
-    }
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+         await axios.get(
+          "http://localhost:3000/me",
+          {
+            withCredentials: true,
+          }
+        );
+
+        setStatus("authenticated");
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          if (error.response?.status === 401) {
+            setStatus("unauthenticated");
+          } else {
+            setStatus("error");
+          }
+        } else {
+          setStatus("error");
+        }
+      }
+    };
+
+    checkUser();
+  }, []);
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "error") {
+    return <p>Server unavailable</p>;
+  }
+
+  if (status === "unauthenticated") {
+    return <Navigate to="/auth/sign-in" replace />;
+  }
+
+  return <>{children}</>;
 }
